9bd399227f77920eb657bdb67be5d11ed8ffd511
Deepesh70 <deepeshdangi700@gmail.com>
2025-12-07 17:54:09 +0530

    fixes

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ const port = 8081;
 const TodoList = ["1","2", "3"];
 
 http
-    .createServer((require, response) => {
-        const { method, url } = require;
+    .createServer((request, response) => {
+        const { method, url } = request;
         
         if(url === "/TodoList"){
             if(method === "GET"){
                 response.writeHead(200,{"Content-Type": "text/html"});
                 response.write(TodoList.toString());
+            } else if(method === "POST"){
+                let body = '';
             }
             else{
-                response.write(404);
+                response.writeHead(404);
             }
         }
         else{
-            response.write(404);    
+            response.writeHead(404);    
         }
 
         response.end();
